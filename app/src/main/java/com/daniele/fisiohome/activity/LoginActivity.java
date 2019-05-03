7c7b465a594ff00c6a1ad18e7d894b54ded00c08
DanieleAndrade <danyelebatista@gmail.com>
2019-05-02 22:05:58 -0300

    Cadastro e login de usuário

--- app/src/main/java/com/daniele/fisiohome/activity/LoginActivity.java
+++ app/src/main/java/com/daniele/fisiohome/activity/LoginActivity.java
@@ -1,40 +1,158 @@
 package com.daniele.fisiohome.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.daniele.fisiohome.R;
+import com.daniele.fisiohome.helper.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button cadastrarButton;
+    private Button botaoAcessar;
+    private Button botaoCadastrar;
+    private EditText campoEmail, campoSenha;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        cadastrarButton = findViewById(R.id.button_cadastrar);
-    }
+        inicializaComponentes();
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        cadastrarButton.setOnClickListener(new View.OnClickListener() {
+        botaoAcessar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cadastroPaciente();
+
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                if (!email.isEmpty()) {
+                    if (!senha.isEmpty()) {
+
+                        autenticacao.signInWithEmailAndPassword(
+                                email, senha
+                        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(LoginActivity.this,
+                                            "Logado com sucesso",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    telaHome();
+
+                                } else {
+                                    Toast.makeText(LoginActivity.this,
+                                            "Erro ao fazer login : " + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+
+                    } else {
+                        Toast.makeText(LoginActivity.this,
+                                "Preencha a senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this,
+                            "Preencha o E-mail!",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = campoEmail.getText().toString();
+                String senha = campoSenha.getText().toString();
+
+                if (!email.isEmpty()) {
+                    if (!senha.isEmpty()) {
+
+                        autenticacao.createUserWithEmailAndPassword(
+                                email, senha
+                        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(LoginActivity.this,
+                                            "Cadastro realizado com sucesso!",
+                                            Toast.LENGTH_SHORT).show();
+
+                                    cadastroPaciente();
+
+                                } else {
+
+                                    String erroExcecao = "";
+
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthWeakPasswordException e) {
+                                        erroExcecao = "Digite uma senha mais forte!";
+                                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                                        erroExcecao = "Por favor, digite um e-mail válido";
+                                    } catch (FirebaseAuthUserCollisionException e) {
+                                        erroExcecao = "Este conta já foi cadastrada";
+                                    } catch (Exception e) {
+                                        erroExcecao = "ao cadastrar usuário: " + e.getMessage();
+                                        e.printStackTrace();
+                                    }
+
+                                    Toast.makeText(LoginActivity.this,
+                                            "Erro: " + erroExcecao,
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
             }
 
+
         });
+
+
     }
 
-    public void cadastroPaciente(){
+    private void inicializaComponentes() {
+        campoEmail = findViewById(R.id.editCadastroEmail);
+        campoSenha = findViewById(R.id.editCadastroSenha);
+        botaoAcessar = findViewById(R.id.buttonAcesso);
+        botaoCadastrar = findViewById(R.id.buttonCadastrar);
+    }
+
+    public void cadastroPaciente() {
         Intent intent = new Intent(this, CadastroPacienteActivity.class);
+        startActivity(intent);
+    }
+
+    public void telaHome() {
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 }
