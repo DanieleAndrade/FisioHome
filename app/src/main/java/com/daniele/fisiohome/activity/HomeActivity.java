5cc2bd56a03345bfdcfa44fd9bd7034a9476b841
Daniele Andrade <danyelebatista@gmail.com>
2019-05-05 22:07:26 -0300

    Telas de Home e Horarios

--- app/src/main/java/com/daniele/fisiohome/activity/HomeActivity.java
+++ app/src/main/java/com/daniele/fisiohome/activity/HomeActivity.java
@@ -1,15 +1,101 @@
 package com.daniele.fisiohome.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.daniele.fisiohome.FisioHome;
 import com.daniele.fisiohome.R;
+import com.daniele.fisiohome.adapters.FisioterapeutasAdapter;
+import com.daniele.fisiohome.model.Fisioterapeuta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ListView listViewFisioterapeutas;
+    ProgressDialog progressDialog;
+    FisioterapeutasAdapter fisioterapeutasAdapter;
+    List<Fisioterapeuta> fisioterapeutasLista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        listViewFisioterapeutas = findViewById(R.id.listview_fisioterapeutas);
+
+        buscarFisioterapeutas();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        listViewFisioterapeutas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                goToDetalhesFisio(fisioterapeutasLista.get(position));
+            }
+        });
+    }
+
+    public void goToDetalhesFisio(Fisioterapeuta fisioterapeuta) {
+        FisioHome.setFisioterapeutaAtual(fisioterapeuta);
+        Intent intent = new Intent(this, DetalheFisioterapeutaActivity.class);
+        startActivity(intent);
+    }
+
+    private void buscarFisioterapeutas() {
+
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, List<Fisioterapeuta>> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = new ProgressDialog(HomeActivity.this);
+                progressDialog.setMessage("Recuperando fisioterapeutas");
+                progressDialog.show();
+            }
+
+            @Override
+            protected List<Fisioterapeuta> doInBackground(String... params) {
+
+                try {
+                    return Fisioterapeuta.getFisioterapeutas();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+
+            protected void onPostExecute(List<Fisioterapeuta> fisioterapeutas) {
+                super.onPostExecute(fisioterapeutas);
+
+                if (fisioterapeutas != null && fisioterapeutas.size() > 0) {
+                    progressDialog.dismiss();
+                    fisioterapeutasAdapter = new FisioterapeutasAdapter(HomeActivity.this, fisioterapeutas);
+                    listViewFisioterapeutas.setAdapter(fisioterapeutasAdapter);
+
+                    fisioterapeutasLista = new ArrayList<Fisioterapeuta>();
+                    fisioterapeutasLista = fisioterapeutas;
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(HomeActivity.this, "Não foi possivel recuperar os fisioterapeutas. " +
+                            "Verifique sua conexão com a internet.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute();
     }
 }
---
+++ app/src/main/java/com/daniele/fisiohome/adapters/HorariosFisioAdapter.java
@@ -0,0 +1,57 @@
+package com.daniele.fisiohome.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.daniele.fisiohome.R;
+import com.daniele.fisiohome.model.Disponibilidade;
+
+import java.util.List;
+
+public class HorariosFisioAdapter extends BaseAdapter {
+    private Context contextView;
+    private LayoutInflater layoutInflater;
+    private List<Disponibilidade> horariosList;
+
+    public HorariosFisioAdapter(Context context, List<Disponibilidade> horariosList) {
+        this.contextView = context;
+        this.horariosList = horariosList;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return horariosList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return horariosList.get(position);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        @SuppressLint("ViewHolder")
+        View v = layoutInflater.inflate(R.layout.item_lista_horarios, parent, false);
+
+        TextView textViewHorariosFisio = v.findViewById(R.id.horario);
+
+        Disponibilidade disponibilidade = horariosList.get(position);
+
+        if(disponibilidade != null && disponibilidade.getHoras() != null) {
+            textViewHorariosFisio.setText(disponibilidade.getHoras());
+        }
+
+        return v;
+    }
+}
