5f2e724e439dba50ef1be9e5b8ed5dc2fb100682
igor-aquiraz <igor.aquiraz@gmail.com>
2019-05-25 13:10:40 -0300

    Melhoria

--- app/src/main/java/com/daniele/fisiohome/activity/HomeActivity.java
+++ app/src/main/java/com/daniele/fisiohome/activity/HomeActivity.java
@@ -1,13 +1,18 @@
 package com.daniele.fisiohome.activity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.daniele.fisiohome.FisioHome;
@@ -31,8 +36,7 @@ public class HomeActivity extends AppCompatActivity {
     FisioterapeutasAdapter fisioterapeutasAdapter;
     List<Fisioterapeuta> fisioterapeutasLista;
     DatabaseReference fisioterapeutasRef;
-
-
+    Button filtroEstado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,9 @@ public class HomeActivity extends AppCompatActivity {
 
         fisioterapeutasLista = new ArrayList<>();
 
-       // buscarFisioterapeutas();
+        filtroEstado = findViewById(R.id.button_estado);
+
+        // buscarFisioterapeutas();
     }
 
     @Override
@@ -53,14 +59,14 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onStart();
 
-        fisioterapeutasRef.addValueEventListener( new ValueEventListener() {
+        fisioterapeutasRef.addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 fisioterapeutasLista.clear();
 
-                for(DataSnapshot fisioterapeutaSnapshot : dataSnapshot.getChildren()) {
+                for (DataSnapshot fisioterapeutaSnapshot : dataSnapshot.getChildren()) {
 
                     Fisioterapeuta fisioterapeuta = fisioterapeutaSnapshot.getValue(Fisioterapeuta.class);
                     fisioterapeutasLista.add(fisioterapeuta);
@@ -89,12 +95,49 @@ public class HomeActivity extends AppCompatActivity {
                 goToDetalhesFisio(fisioterapeutasLista.get(position));
             }
         });
+
+        filtroEstado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                filtrarEstado(v);
+            }
+        });
     }
 
     public void goToDetalhesFisio(Fisioterapeuta fisioterapeuta) {
         FisioHome.setFisioterapeutaAtual(fisioterapeuta);
         Intent intent = new Intent(this, DetalheFisioterapeutaActivity.class);
         startActivity(intent);
+    }
+
+    public void filtrarEstado(View view) {
+        AlertDialog.Builder dialogEstado = new AlertDialog.Builder(this);
+        dialogEstado.setTitle("Selecione o estado desejado");
+
+        View viewSpinner = getLayoutInflater().inflate(R.layout.dialog_spinner, null);
+        Spinner spinnerEstado = viewSpinner.findViewById(R.id.spinner_filtro);
+        dialogEstado.setView(viewSpinner);
+
+        String[] estados = getResources().getStringArray(R.array.ufs);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, estados);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerEstado.setAdapter(adapter);
+
+        dialogEstado.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        dialogEstado.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = dialogEstado.create();
     }
 
 //    private void buscarFisioterapeutas() {
